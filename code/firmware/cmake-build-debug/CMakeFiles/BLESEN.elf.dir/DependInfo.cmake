9ad573a29b81f5091d67fc5fa807ee367abf22c0
Jan Eichhorn <exeu65@googlemail.com>
2024-05-27 06:44:38 +0200

    Initial commit

---
+++ code/firmware/cmake-build-debug/CMakeFiles/BLESEN.elf.dir/DependInfo.cmake
@@ -0,0 +1,119 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Startup/startup_stm32wb55ceux.s" "D:/Elektronik/Kicad/BLESEN/code/firmware/cmake-build-debug/CMakeFiles/BLESEN.elf.dir/Core/Startup/startup_stm32wb55ceux.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32WB55xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Inc"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/STM32_WPAN/App"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Inc"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Inc/Legacy"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Utilities/lpm/tiny_lpm"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/utilities"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/template"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/svc/Inc"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/svc/Src"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/CMSIS/Device/ST/STM32WBxx/Include"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Utilities/sequencer"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/app_debug.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/app_debug.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/app_debug.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/app_entry.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/app_entry.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/app_entry.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/hw_timerserver.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/hw_timerserver.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/hw_timerserver.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/main.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/stm32_lpm_if.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32_lpm_if.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32_lpm_if.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/stm32wbxx_hal_msp.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32wbxx_hal_msp.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32wbxx_hal_msp.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/stm32wbxx_it.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32wbxx_it.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/stm32wbxx_it.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/syscalls.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/sysmem.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Core/Src/system_stm32wbxx.c" "CMakeFiles/BLESEN.elf.dir/Core/Src/system_stm32wbxx.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Core/Src/system_stm32wbxx.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_adc_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_cortex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_cortex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_cortex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_exti.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_exti.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_exti.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_flash_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_gpio.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_gpio.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_gpio.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_hsem.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_hsem.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_hsem.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_i2c_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_ipcc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_ipcc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_ipcc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rtc_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_tim_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart_ex.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_uart_ex.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_adc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_adc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_adc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_rcc.c" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_rcc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Drivers/STM32WBxx_HAL_Driver/Src/stm32wbxx_ll_rcc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gap_aci.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gap_aci.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gap_aci.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hal_aci.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hci_le.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hci_le.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_hci_le.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_l2cap_aci.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_l2cap_aci.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_l2cap_aci.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/svc/Src/dis.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/dis.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/dis.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/svc/Src/hts.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/hts.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/hts.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/ble/svc/Src/svc_ctl.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/svc_ctl.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/ble/svc/Src/svc_ctl.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci/shci.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci/shci.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci/shci.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl_if.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl_if.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl_if.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl_if.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl_if.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/shci_tl_if.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl_mbox.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl_mbox.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl_mbox.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/utilities/dbg_trace.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/dbg_trace.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/dbg_trace.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/utilities/otp.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/otp.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/otp.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/utilities/stm_list.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/stm_list.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/stm_list.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Middlewares/ST/STM32_WPAN/utilities/stm_queue.c" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/stm_queue.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Middlewares/ST/STM32_WPAN/utilities/stm_queue.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/STM32_WPAN/App/app_ble.c" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/app_ble.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/app_ble.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/STM32_WPAN/App/dis_app.c" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/dis_app.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/dis_app.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/STM32_WPAN/App/hts_app.c" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/hts_app.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/App/hts_app.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/STM32_WPAN/Target/hw_ipcc.c" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/Target/hw_ipcc.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/STM32_WPAN/Target/hw_ipcc.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Utilities/lpm/tiny_lpm/stm32_lpm.c" "CMakeFiles/BLESEN.elf.dir/Utilities/lpm/tiny_lpm/stm32_lpm.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Utilities/lpm/tiny_lpm/stm32_lpm.c.obj.d"
+  "D:/Elektronik/Kicad/BLESEN/code/firmware/Utilities/sequencer/stm32_seq.c" "CMakeFiles/BLESEN.elf.dir/Utilities/sequencer/stm32_seq.c.obj" "gcc" "CMakeFiles/BLESEN.elf.dir/Utilities/sequencer/stm32_seq.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/firmware/cmake-build-debug/CMakeFiles/BLESEN.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BLESEN.elf.
+# This may be replaced when dependencies are built.
